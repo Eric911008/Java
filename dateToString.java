d7e986d02a89f97a7da2d7c6c029aee0d4c418d6
eric1008 <93502596+Eric911008@users.noreply.github.com>
2022-03-16 22:27:24 +0800

    Add files via upload

---
+++ dateToString.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+public class dateToString
+{
+    public static void main(String[] args)
+    {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(date);
+        System.out.println("Converted String: " + strDate);
+
+    }
+}
